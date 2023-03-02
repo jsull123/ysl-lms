3efe9e248d5a9947fae54c9b75c0df1a6af0c59e
Khalil <khalilgj@email.sc.edu>
2023-03-02 12:31:06 -0500

    Stubs for the Comment class

---
+++ source/Comment.java
@@ -0,0 +1,29 @@
+import java.util.UUID;
+import java.util.Date;
+
+private UUID authorID;
+private String comment;
+private Date dateAdded;
+private ArrayList<Comment> comments;
+
+public Comment(UUID authorID, float rating, String review, Date dateAdded){
+    this.dateAdded = dateAdded;
+    this.UUID = authorID;
+    this.comment = review;
+    comments.add(review);
+}
+
+
+public int numComments(){
+    return 1;
+}
+
+public Comment getComment(int index) {
+    
+    Comment CommentOfInterest = comments.get(0);
+    return CommentOFInterest;
+}
+
+public void toString(){
+    System.out.pritnln("Stub");
+}
