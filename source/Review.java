5645868cfdfa7da3d75214611b0f21db3525daee
Jakub Sykora <jakubsykora112@gmail.com>
2023-03-02 12:12:08 -0500

    Add stubs for Review.java

---
+++ source/Review.java
@@ -0,0 +1,24 @@
+package source;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Review { 
+
+    private UUID authorID;
+    private float rating;
+    private String review;
+    private Date dateAdded;
+
+    public Review(UUID authorID, float rating, String review, Date dateAdded) {
+        this.authorID = authorID;
+        this.rating = rating;
+        this.review = review;
+        this.dateAdded = dateAdded;
+    }
+
+    public String toString() {
+        String returnString = "";
+        return  returnString;
+    }
+}
