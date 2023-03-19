df2396c15e4e05fda68cb14add4f4d857812415b
Jakub Sykora <jakubsykora112@gmail.com>
2023-03-19 15:49:53 -0400

    Add missing jsonparser as well

--- source/CourseDataProcessor.java
+++ source/CourseDataProcessor.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.UUID;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class CourseDataProcessor {
 
