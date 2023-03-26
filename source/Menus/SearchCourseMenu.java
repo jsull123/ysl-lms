d5fb7f2d14347191811b92d89f585b9ffe7d4dff
Khalil <khalilgj@email.sc.edu>
2023-03-26 19:26:36 -0400

    so i can pull

--- source/Menus/SearchCourseMenu.java
+++ source/Menus/SearchCourseMenu.java
@@ -4,5 +4,16 @@ import source.LMSUI;
 
 public class SearchCourseMenu extends Menu {
 
+    public SearchCourseMenu(LMSFacade facade){
+        header = "Search a course by its language:";
+        options = new String[]{"Java", "C", "Python"};
+        this.facade = facade;
+    }
     
+    public void select(int selection){
+        switch (selection){
+            case 1:
+                
+        }
+    }
 }
