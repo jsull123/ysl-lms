71309bd5e80df7dff93d355438ed345d78f7d3bb
Jakub Sykora <jakubsykora112@gmail.com>
2023-03-28 12:49:26 -0400

    Add openModule menu that will replace takecontent after module.java changes

---
+++ source/Menus/OpenModule.java
@@ -0,0 +1,16 @@
+package source.Menus;
+import java.util.ArrayList;
+import source.EnrolledCourse;
+import source.Question;
+import source.LMSFacade;
+
+public class OpenModule extends ListMenu{
+
+    private Module module;
+    private ArrayList<Question> quiz;
+    private ArrayList<String> lessons;
+    public OpenModule(Facade facade, Menu pMenu, Module module, EnrolledCourse course) {
+        this.list = module.get
+    }
+    
+}
