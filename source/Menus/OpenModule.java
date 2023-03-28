b176d647e5c092a0e710eb2bdceb407cd0c8ede3
Jakub Sykora <jakubsykora112@gmail.com>
2023-03-28 12:53:45 -0400

    Create constructor

--- source/Menus/OpenModule.java
+++ source/Menus/OpenModule.java
@@ -3,14 +3,16 @@ import java.util.ArrayList;
 import source.EnrolledCourse;
 import source.Question;
 import source.LMSFacade;
+import source.Module;
 
 public class OpenModule extends ListMenu{
 
     private Module module;
     private ArrayList<Question> quiz;
-    private ArrayList<String> lessons;
-    public OpenModule(Facade facade, Menu pMenu, Module module, EnrolledCourse course) {
-        this.list = module.get
+    public OpenModule(LMSFacade facade, Menu pMenu, Module module, EnrolledCourse course) {
+        super(facade, pMenu, module.getLessons());
+        quiz = module.getQuiz();
+        this.module = module;
     }
     
 }
