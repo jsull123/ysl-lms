4d279ae4b2384b02f306c3c7985490c22e733687
Jakub Sykora <jakubsykora112@gmail.com>
2023-04-09 20:52:36 -0400

    Add same username and same UUID tests

--- source/TestUserDataProcessor.java
+++ source/TestUserDataProcessor.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class TestUserDataProcessor {
+class TestUserDataProcessor {
 
     private static ArrayList<User> users;
     private static UserList userList;
@@ -24,7 +24,7 @@ public class TestUserDataProcessor {
     }
 
     @AfterEach
-    public static void tearDown() {
+    public void tearDown() {
         users.clear();
     }
 
@@ -55,27 +55,8 @@ public class TestUserDataProcessor {
             new ArrayList<>(),
             new ArrayList<>()
         ));
-
         UserDataProcessor.saveData(userList);
-        userList.clear();
 	}
-
-    @Test
-    public static void testUserNum() {
-        userList.addUser(new User(
-        UUID.randomUUID(),
-        AccountType.fromInt(0), 
-        "firstname",
-        "lastname",
-        "testStudent1",
-        "testStudent1@gmail.com",
-        "pass123",
-        Date.fromString("04/22/1996"),
-        new ArrayList<>(),
-        new ArrayList<>()
-    ));
-        assertEquals(3, userList.numUsers());
-    }
 
     @AfterAll
     public static void oneTimeTearDown() {
@@ -83,5 +64,116 @@ public class TestUserDataProcessor {
         userList.add(users);
         UserDataProcessor.saveData(userList);
     }
+
+    @Test
+    public void testUserNum() {
+        assertEquals(2, userList.numUsers());
+    }
+
+    @Test
+    public void testAddUser() {
+        UUID userID = UUID.randomUUID();
+        userList.addUser(new User(
+            userID,
+            AccountType.fromInt(0), 
+            "firstname",
+            "lastname",
+            "testStudent1",
+            "testStudent1@gmail.com",
+            "pass123",
+            Date.fromString("04/22/1996"),
+            new ArrayList<>(),
+            new ArrayList<>()
+        ));
+        assertEquals(userID, userList.getUser(userID).getID());
+    }
+
+    @Test
+    public void testZeroUsers() {
+        userList.clear();
+        assertEquals(0, userList.numUsers());
+    }
+
+    @Test
+    public void testNullUserID() {
+        UUID userID = null;
+        userList.addUser(new User(
+            userID,
+            AccountType.fromInt(0), 
+            "firstname",
+            "lastname",
+            "testStudentnull",
+            "testStudent1@gmail.com",
+            "pass123",
+            Date.fromString("06/01/1999"),
+            new ArrayList<>(),
+            new ArrayList<>()
+        ));
+        assertNotNull(userList.getUser("testStudentnull").getID());
+    }
+
+    @Test
+    public void testUsersWithSameID() {
+        UUID userID = UUID.randomUUID();
+        userList.addUser(new User(
+            userID,
+            AccountType.fromInt(0), 
+            "firstname",
+            "lastname",
+            "testStudentSame1",
+            "testStudent1@gmail.com",
+            "pass123",
+            Date.fromString("06/01/1995"),
+            new ArrayList<>(),
+            new ArrayList<>()
+        ));
+        userList.addUser(new User(
+            userID,
+            AccountType.fromInt(0), 
+            "firstname",
+            "lastname",
+            "testStudentSame2",
+            "testStudent1@gmail.com",
+            "pass123",
+            Date.fromString("06/09/1995"),
+            new ArrayList<>(),
+            new ArrayList<>()
+        ));
+        assertEquals(4, userList.numUsers());
+    }
+
+    @Test
+    public void testUsersWithSameUserName() {
+        String username = "sameUsername";
+        UUID first = UUID.randomUUID();
+        UUID second = UUID.randomUUID();
+        userList.addUser(new User(
+            first,
+            AccountType.fromInt(0), 
+            "firstname",
+            "lastname",
+            username,
+            "testStudent1@gmail.com",
+            "pass123",
+            Date.fromString("06/01/1995"),
+            new ArrayList<>(),
+            new ArrayList<>()
+        ));
+        userList.addUser(new User(
+            second,
+            AccountType.fromInt(0), 
+            "firstname",
+            "lastname",
+            username,
+            "testStudent1@gmail.com",
+            "pass123",
+            Date.fromString("06/09/1995"),
+            new ArrayList<>(),
+            new ArrayList<>()
+        ));
+        assertEquals(4, userList.numUsers());
+    }
+
+
 
 }
